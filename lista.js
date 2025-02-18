b1f717baab06ed8850fa6788ac3c5752a25b0ef5
Israel-ops-cmd <israellipe2020@gmail.com>
2025-02-18 14:40:53 -0300

    criando a lista de tarefas

--- lista.js
+++ lista.js
@@ -1,1 +1,8 @@
-const lista = [];
+const tasks = [
+    {descricao: 'Passear com o cachorro', prioridade: 1, status: 'concluída'},
+    {descricao: 'Comprar ingredientes para o almoço', prioridade: 1, status: 'concluída'},
+    {descricao: 'Fazer o almoço', prioridade: 1, status: 'pendente'},
+    {descricao: 'Ir para a academia', prioridade: 2, status: 'pendente'},
+    {descricao: 'Resolver exercício do curso', prioridade: 1, status: 'concluída'},
+    {descricao: 'Ir para o futebol', prioridade: 3, status: 'pendente'}
+]
